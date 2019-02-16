8a6af73b84d9748b3e32220695f8569fc0db3952
Richard Hayes <richardhayes777@gmail.com>
2019-02-16 11:27:05 +0000

    tracks and caching

--- cli.py
+++ cli.py
@@ -26,6 +26,7 @@ def pprint(response_dict):
 class User(object):
     def __init__(self, href):
         self.href = href
+        self.__play_lists = None
 
     @classmethod
     def from_dict(cls, user_dict):
@@ -33,8 +34,10 @@ class User(object):
 
     @property
     def play_lists(self):
-        response = api.make_request("{}/playlists".format(profile_data["href"]))
-        return list(map(PlayList.from_dict, response["items"]))
+        if self.__play_lists is None:
+            response = api.make_request("{}/playlists".format(profile_data["href"]))
+            self.__play_lists = list(map(PlayList.from_dict, response["items"]))
+        return self.__play_lists
 
     def playlist_with_name(self, playlist_name):
         return [playlist for playlist in self.play_lists if playlist.name == playlist_name][0]
@@ -44,6 +47,7 @@ class PlayList(object):
     def __init__(self, name, tracks_url):
         self.name = name
         self.tracks_url = tracks_url
+        self.__tracks = None
 
     @classmethod
     def from_dict(cls, playlist_dict):
@@ -54,12 +58,66 @@ class PlayList(object):
 
     @property
     def tracks(self):
-        return api.make_request(watford_gap.tracks_url)
+        if self.__tracks is None:
+            self.__tracks = list(map(PlayListTrack.from_dict, api.make_request(watford_gap.tracks_url)["items"]))
+        return self.__tracks
+
+    def __getitem__(self, item):
+        return self.tracks[item]
+
+
+class PlayListTrack(object):
+    def __init__(self, track, added_at, added_by):
+        self.track = track
+        self.added_at = added_at
+        self.added_by = added_by
+
+    @classmethod
+    def from_dict(cls, playlist_track_dict):
+        return PlayListTrack(Track.from_dict(playlist_track_dict["track"]),
+                             playlist_track_dict["added_at"],
+                             playlist_track_dict["added_by"])
+
+
+class Track(object):
+    def __init__(self, name, artist_hrefs):
+        self.name = name
+        self.artist_hrefs = artist_hrefs
+
+    @classmethod
+    def from_dict(cls, track_dict):
+        return Track(track_dict["name"],
+                     [artist["href"] for artist in track_dict["artists"]])
+
+    @property
+    def artists(self):
+        return [Artist.from_href(href) for href in self.artist_hrefs]
+
+    @property
+    def genres(self):
+        return {genre for artist in self.artists for genre in artist.genres}
+
+
+class Artist(object):
+    def __init__(self, name, genres):
+        self.name = name
+        self.genres = genres
+
+    href_cache = {}
+
+    @classmethod
+    def from_href(cls, href):
+        if href not in Artist.href_cache:
+            Artist.href_cache[href] = Artist.from_dict(api.make_request(href))
+        return Artist.href_cache[href]
+
+    @classmethod
+    def from_dict(cls, artist_dict):
+        return Artist(artist_dict["name"], artist_dict["genres"])
 
 
 user_profile_api_endpoint = "{}/me".format(config.SPOTIFY_API_URL)
 profile_data = api.make_request(user_profile_api_endpoint)
 user = User(profile_data)
-watford_gap = user.playlist_with_name("Watford Gap (Service station archives)")
 
-pprint(watford_gap.tracks)
+watford_gap = user.playlist_with_name("Watford Gap (Service station archives)")
