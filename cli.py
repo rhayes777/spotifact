cb087fb47ccf499eb67cbc51b6d176fc937f20a3
Richard Hayes <richardhayes777@gmail.com>
2019-02-16 10:25:05 +0000

    model

--- cli.py
+++ cli.py
@@ -4,64 +4,62 @@ import requests
 
 import config
 
+
+class API(object):
+    def __init__(self, access_token):
+        self.access_token = access_token
+        self.authorization_header = {"Authorization": "Bearer {}".format(access_token)}
+
+    def make_request(self, endpoint):
+        response = requests.get(endpoint, headers=self.authorization_header)
+        return json.loads(response.text)
+
+
 with open(config.CREDENTIALS_FILE) as f:
-    access_token = json.loads(f.read())["access_token"]
-
-authorization_header = {"Authorization": "Bearer {}".format(access_token)}
-
-
-def make_request(endpoint):
-    response = requests.get(endpoint, headers=authorization_header)
-    return json.loads(response.text)
+    api = API(json.loads(f.read())["access_token"])
 
 
 def pprint(response_dict):
     print(json.dumps(response_dict, indent=4))
 
 
+class User(object):
+    def __init__(self, href):
+        self.href = href
+
+    @classmethod
+    def from_dict(cls, user_dict):
+        return User(user_dict["href"])
+
+    @property
+    def play_lists(self):
+        response = api.make_request("{}/playlists".format(profile_data["href"]))
+        return list(map(PlayList.from_dict, response["items"]))
+
+    def playlist_with_name(self, playlist_name):
+        return [playlist for playlist in self.play_lists if playlist.name == playlist_name][0]
+
+
+class PlayList(object):
+    def __init__(self, name, tracks_url):
+        self.name = name
+        self.tracks_url = tracks_url
+
+    @classmethod
+    def from_dict(cls, playlist_dict):
+        return PlayList(playlist_dict["name"], playlist_dict["tracks"]["href"])
+
+    def __repr__(self):
+        return self.name
+
+    @property
+    def tracks(self):
+        return api.make_request(watford_gap.tracks_url)
+
+
 user_profile_api_endpoint = "{}/me".format(config.SPOTIFY_API_URL)
-profile_data = make_request(user_profile_api_endpoint)
+profile_data = api.make_request(user_profile_api_endpoint)
+user = User(profile_data)
+watford_gap = user.playlist_with_name("Watford Gap (Service station archives)")
 
-playlist_api_endpoint = "{}/playlists".format(profile_data["href"])
-playlist_data = make_request(playlist_api_endpoint)
-
-pprint(playlist_data)
-
-
-"""
-{
-    "collaborative": false,
-    "external_urls": {
-        "spotify": "https://open.spotify.com/playlist/0uDn2TE7eouHnAZPHLXtF7"
-    },
-    "href": "https://api.spotify.com/v1/playlists/0uDn2TE7eouHnAZPHLXtF7",
-    "id": "0uDn2TE7eouHnAZPHLXtF7",
-    "images": [
-        {
-            "height": null,
-            "url": "https://pl.scdn.co/images/pl/default/cc5e818ac8330461fafc532a690d0f93463d85a6",
-            "width": null
-        }
-    ],
-    "name": "Watford Gap (Service station archives)",
-    "owner": {
-        "display_name": "amsilverwood",
-        "external_urls": {
-            "spotify": "https://open.spotify.com/user/amsilverwood"
-        },
-        "href": "https://api.spotify.com/v1/users/amsilverwood",
-        "id": "amsilverwood",
-        "type": "user",
-        "uri": "spotify:user:amsilverwood"
-    },
-    "primary_color": null,
-    "public": true,
-    "snapshot_id": "MTMxLDgzZTU3YTFjM2ZlNmQyOTVhYTMxMGIyNmQ4YTU0YWI4ZTUwMTYzYzM=",
-    "tracks": {
-        "href": "https://api.spotify.com/v1/playlists/0uDn2TE7eouHnAZPHLXtF7/tracks",
-        "total": 1002
-    },
-    "type": "playlist",
-            "uri": "spotify:user:amsilverwood:playlist:0uDn2TE7eouHnAZPHLXtF7"
-}
-"""
+pprint(watford_gap.tracks)
